a76aff8c45971d00905ff162a89da655c61680ac
Leonard Yeo <leonardyeo88@gmail.com>
2016-02-20 14:55:35 +0800

    updates

--- ProProperty/Controllers/PropertyController.cs
+++ ProProperty/Controllers/PropertyController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProProperty.DAL;
+using ProProperty.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -50,7 +52,14 @@ namespace ProProperty.Controllers
 
 
             ViewBag.PremiseType = premiseType;
-            
+            //sample data for testing 
+            var property1 = new Property();
+            property1.property_id = 1;
+            property1.address = "ang mo kio ave 10";
+            property1.latitude = 1.3699034m;
+            property1.longtitude = 103.8454906m;
+            property1.type = "HDB";
+
             return View();
         }
 
