2e66c7978b3a60fd638d831c87d6fc0f0d759470
antonsalim <anton_salim@hotmail.com>
2016-02-22 08:18:54 +0800

    ok

---
+++ ProProperty/Models/Hdb_price_range.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace ProProperty.Models
+{
+    [Table("Hdb_price_range")]
+    public class Hdb_price_range
+    {
+        [HiddenInput(DisplayValue = false)]
+        [Key]
+        public int _id { get; set; }
+        public string financial_year { get; set; }
+        public string town { get; set; }
+        public string room_type { get; set; }
+        public string min_selling_price {get; set;}
+        public string max_selling_price { get; set; }
+        public string min_selling_price_less_ahg_shg { get; set; }
+        public string max_selling_price_less_ahg_shg { get; set; }
+    }
+}
