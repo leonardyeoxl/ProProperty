9a76761b7ebe045accac3eac5a35c8061367c4a7
antonsalim <anton_salim@hotmail.com>
2016-03-22 07:25:48 +0800

    Added agent

--- ProProperty/Models/PropertyWithPremises.cs
+++ ProProperty/Models/PropertyWithPremises.cs
@@ -8,6 +8,7 @@ namespace ProProperty.Models
     public class PropertyWithPremises
     {
         public Property property { get; set; }
+        public Agent agent { get; set; }
         public List<Premise> listOfPremise { get; set; }
     }
 }
