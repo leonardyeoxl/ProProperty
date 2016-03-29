391927a8b06c0f9b1408129bd9a2179cba38613f
HiuFungC <14SIC012U@sit.singaporetech.edu.sg>
2016-03-29 15:11:13 +0800

    updated with index javascript

--- ProProperty/Scripts/googleApi.js
+++ ProProperty/Scripts/googleApi.js
@@ -1,42 +1,48 @@
-﻿var map;
-function createPropertyMarker(id, address, lat, lng, type) {
+﻿var Story =
+{
+    _urlFormat: '',
+    _urlHello: '',
+
+    init: function (urlFormat,urlHello) {
+        Story._urlFormat = urlFormat;
+        window.alert(urlHello);
+        window.alert(urlFormat);
+        function InitializeMap() {
+
+            var latlng = new google.maps.LatLng(1.373334, 103.835518);
+            var myOptions = {
+                zoom: 5,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(document.getElementById("map_canvas"), myOptions)
+        }
+        window.onload = InitializeMap;
+    }
+}
+
+var Marker =
+{
+    _markerId:'',
+    _markerAddress: '',
+    _markerLat:'',
+    _markerLong:'',
+    _markerType: '',
+    _LinkPropertyDetails:'',
+    _LinkPropertyInformation:'',
+    Create: function (map,markerId, markerAddress, markerLat, markerLong, marketType, LinkPropertyDetails, LinkPropertyInformation)
+    {
+        var image = returnIcon(marketType);
+        createMarker(map,markerId, markerAddress, markerLat, markerLong, marketType, LinkPropertyDetails, LinkPropertyInformation);
+    }
+}
+
+//var map;
+function createMarker(map,id, address, lat, lng, type, LinkPropertyDetails, LinkPropertyInformation) {
     console.log(id + lat);
     console.log(lng);
     console.log(type);
     var image = returnIcon(type);
-    console.log(image);
-    var marker = new google.maps.Marker
-    (
-        {
-            position: new google.maps.LatLng(lat, lng),
-            map: map,
-            icon: image,
-            title: address
-        }
-    );
-    var cityCircle = new google.maps.Circle({
-        strokeColor: '#00ff7f',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#00ff7f',
-        fillOpacity: 0.35,
-        map: map,
-        center: new google.maps.LatLng(lat, lng),
-        radius: 1600
-    });
-    //for clicking on the marker
-    var infowindow = new google.maps.InfoWindow({
-        content: address
-    });
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
-    });
-}
-function createPremisesMarker(id, address, lat, lng, type) {
-    console.log(id + lat);
-    console.log(lng);
-    console.log(type);
-    var image = returnPremises(type);
     console.log(image);
     var marker = new google.maps.Marker
         (
@@ -49,7 +55,7 @@ function createPremisesMarker(id, address, lat, lng, type) {
         );
     //for clicking on the marker
     var infowindow = new google.maps.InfoWindow({
-        content: address
+        content: address + '</br><a href="' + LinkPropertyDetails + '"> View property </a></br><a href="' + LinkPropertyInformation + '"> View Information </a>'
     });
     google.maps.event.addListener(marker, 'click', function () {
         infowindow.open(map, marker);
@@ -66,61 +72,4 @@ function returnIcon(type) {
         return '/Image/icon/landed.png';
     }
 }
-function returnPremises(type){
-    if(type==1){
-        return '/Image/icon/clinic.png';
-    }
-    if(type==2){
-        return '/Image/icon/dining.png';
-    }
-    if(type==3){
-        return '/Image/icon/gym.png';
-    }
-    if(type==4){
-        return '/Image/icon/highway.png';
-    }
-    if(type==5){
-        return '/Image/icon/landed.png';
-    }
-    if(type==6){
-        return '/Image/icon/park.png';
-    }
-    if(type==7){
-        return '/Image/icon/parking.png';
-    }
-    if(type==8){
-        return '/Image/icon/school.png';
-    }
-    if(type==9){
-        return '/Image/icon/train.png';
-    }
-}
-function InitializeMap() {
 
-    var latlng = new google.maps.LatLng(1.373334, 103.835518);
-    var myOptions = {
-        zoom: 11,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    ////createMarker();
-    //@if (Model != null)
-    //{
-    //    <text>createPropertyMarker(@Html.DisplayFor(modeitem => Model.property.propertyID), "@Html.DisplayFor(model => model.property.address)", @Html.DisplayFor(model => model.property.Latitude), @Html.DisplayFor(model => model.property.Longitude), "@Html.DisplayFor(model => model.property.propertyType)");</text>
-    //    //console.log("got model");
-    //    foreach (var item in Model.listOfPremise)
-    //    {
-    //        <text>createPremisesMarker(@Html.DisplayFor(modeitem => item.premises_id), "@Html.DisplayFor(model => item.premises_address)", @Html.DisplayFor(model => item.premises_lat), @Html.DisplayFor(model => item.premises_long), "@Html.DisplayFor(model => item.premises_type_id)");</text>
-    //    }
-    //}
-
-
-}
-window.onload = InitializeMap;
-
-function testHello() {
-    window.alert("hello");
-    document.getElementById("demo").innerHTML="hello";
-    return true;
-}
